d9c7892e0bb41db7f7ee2c99e37de49d1212ff35
Carolyn Whelpley <carolynwhelpley@carolyns-air.lan>
2021-02-05 12:28:19 -0500

    added new data

--- covid.py
+++ covid.py
@@ -2,7 +2,7 @@
 import csv
 import matplotlib.pyplot as plt
 
-filename = '/Users/carolynwhelpley/covid/data/CasesByDate.csv'
+filename = '/Users/carolynwhelpley/covid/data/massachusetts-history.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -12,19 +12,24 @@ with open(filename) as f:
 
     cases = []
     for row in reader:
-        print(row[3])
-        case = (row[2])
-        cases.append(case)
-
-    
+        print(row[4])
+        print(".......")
+        if (row[4]) != 0:
+            case = (int(row[4]))
+            cases.append(case)
+        print(cases)
+        
 
     plt.style.use('default')
     fig, ax = plt.subplots()
-    ax.plot(cases, c='red')
+    ax.plot(cases, 'bo')
+    #plt.axis([0, 200, 0, 7000])
+    
 
     plt.title("Daily cases for MA, covid 19", fontsize=24)
     plt.xlabel(" ", fontsize=16)
     plt.ylabel("Case number", fontsize=16)
-    plt.tick_params(axis='both', which='major', labelsize=16)
+    plt.tick_params(axis='y', which='major', labelsize=16)
+    #ax.set_ylim(0, 7000)
 
     plt.show()
